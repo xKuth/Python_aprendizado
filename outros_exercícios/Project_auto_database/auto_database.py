18c33a9f05d3c1b4695aff1d83ff6f088afee636
xKuth <luisyeager20@gmail.com>
2024-12-11 18:15:08 -0300

    aprendendo a usar select no python via mysql
    puxando todos dados da table no python com comandos sql

--- outros_exercícios/Project_auto_database/auto_database.py
+++ outros_exercícios/Project_auto_database/auto_database.py
@@ -11,11 +11,24 @@ connect = mysql.connector.connect(
 table = connect.cursor()
 # table.execute("alter table game add column valores int (3)"
 # table.execute("alter table game modify column valores float(3) not null")
+# table.execute("alter table game add column comp_validada varchar(1) not null")
+'''
 for i in range(1, 200):
     num_aleatorio = random.uniform(0, 100)
     table.execute(f"UPDATE game set valores = (%s) where id = (%s)", (num_aleatorio, i))
+'''
 
-connect.commit()
+'''
+for i in range(0, 200):
+    escolhida = random.choice(['v', 'f'])
+    table.execute("Update game set comp_validada = (%s) where id = (%s)", (escolhida, i))
+'''
+table.execute("select * from game")
+resultado = table.fetchall()
+for x in resultado:
+    print(x)
+
+# connect.commit()
 
 table.close()
 connect.close()
